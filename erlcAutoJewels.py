c5c8881f45461ca0b45a2f5ee63015a57a29bd40
TheBestMinecraftPlayerFromWorld <85893346+TheBestMinecraftPlayerFromWorld@users.noreply.github.com>
2024-08-11 15:42:43 +0200

    Update erlcAutoJewels.py

--- erlcAutoJewels.py
+++ erlcAutoJewels.py
@@ -1,4 +1,4 @@
-import subprocess,time,keyboard
+import subprocess
 
 # Assuming tinytask.exe and your .rec file are in the same directory
 subprocess.run(["C:\\Users\\TBMPFW\\Desktop\\programme\\autoRun\\tinytask-1-77.exe", "C:\\Users\\TBMPFW\\Desktop\\programme\\autoRun\\autoJewels.rec"])
